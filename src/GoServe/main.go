6097ff7e0d10967aa9b75edd30ada023a76d3ded
Patrick William Haugen <pathaugen@gmail.com>
2016-06-23 02:36:40 -0700

    Webserving while console active
    Webserving while console active

--- src/GoServe/main.go
+++ src/GoServe/main.go
@@ -8,11 +8,22 @@ import (
 	"fmt"
 	//"os"
 	//"strings"
+	"net/http"
 )
 
 
 var exit bool = false
 var version string = "1.0.0"
+
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("\n\n")
+	fmt.Print("function handler triggered")
+	fmt.Print("\n\n")
+	
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 
 func main() {
@@ -32,14 +43,41 @@ func main() {
 	
 	
 	
+	fmt.Print("Starting Webserver on port 8080")
+	//http.HandleFunc("/", handler)
+    //http.ListenAndServe(":8080", nil)
+    server8080 := http.NewServeMux()
+    server8080.HandleFunc("/", handler)
+    go func() {
+		http.ListenAndServe(":8080", server8080)
+    }()
+	fmt.Print("\n\n")
+	fmt.Print("Webserver STARTED on port 8080")
+	fmt.Print("\n\n")
+	
+	
+	
+	// Every 10 seconds save log files
+	// xxx
+	
+	
 	for !exit {
 		waitForInput()
 	}
 	
 	
 	if exit {
+		// Save command log file: goserve-commands.log
+		// xxx
+		
+		// Save visitor log file: goserve-visitors.log
+		// xxx
+		
 		exitHandler()
 	}
+	
+	
+    
 	
 }
 

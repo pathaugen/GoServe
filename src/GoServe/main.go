283fe60f3a6f26899b397081a023764c44eef1c4
Patrick William Haugen <pathaugen@gmail.com>
2016-06-23 00:47:29 -0700

    Splash Screen
    Splash Screen

--- src/GoServe/main.go
+++ src/GoServe/main.go
@@ -4,17 +4,32 @@ package main
 
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
-	"os"
+	//"os"
 	//"strings"
 )
 
 
 var exit bool = false
+var version string = "1.0.0"
 
 
 func main() {
+	
+	// http://patorjk.com/software/taag/#p=display&v=1&f=Big&t=GoServe!
+	fmt.Print("   _____       _____                     _", "\n")
+	fmt.Print("  / ____|     / ____|                   | |", "\n")
+	fmt.Print(" | |  __  ___| (___   ___ _ ____   _____| |", "\n")
+	fmt.Print(" | | |_ |/ _ \\\\___ \\ / _ \\ '__\\ \\ / / _ \\ |", "\n")
+	fmt.Print(" | |__| | (_) |___) |  __/ |   \\ V /  __/_|", "\n")
+	fmt.Print("  \\_____|\\___/_____/ \\___|_|    \\_/ \\___(_)")
+	fmt.Print("\n\n")
+	fmt.Print("Version : ", version)
+	fmt.Print("\n")
+	fmt.Print("https://github.com/pathaugen/GoServe")
+	fmt.Print("\n\n")
+	
 	
 	
 	for !exit {
@@ -23,17 +38,7 @@ func main() {
 	
 	
 	if exit {
-		fmt.Print("\n\n")
-		fmt.Print("Application is exiting. Press [enter] to continue.")
-		fmt.Print("\n\n")
-		fmt.Print("[exiting] > ")
-		
-		consolereader := bufio.NewReader(os.Stdin)
-		_, err := consolereader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		exitHandler()
 	}
 	
 }

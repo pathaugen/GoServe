db3910a1d5dfcec85a7eb292918e5610525029bd
Patrick William Haugen <pathaugen@gmail.com>
2016-06-24 18:35:42 -0700

    WIP
    WIP

--- src/GoServe/main.go
+++ src/GoServe/main.go
@@ -21,10 +21,13 @@ import (
 )
 
 
+// ========== ========== ========== ========== ==========
+// Variables spanning across all .go files and functions in application
 var exit		bool	= false
 var version		string	= "1.0.0"
 var monitoring	bool	= false		// Console based traffic monitoring switch (configured via flag or console)
 var port		string	= "8080"	// Port to serve web requests on (configured via flag or console)
+// ========== ========== ========== ========== ==========
 
 
 func main() {
@@ -34,7 +37,6 @@ func main() {
 	fmt.Print(color.RedString("This is a test of Color"))
 	fmt.Print("\n\n")
 	*/
-	
 	
 	consoleColor := ansicolor.NewAnsiColorWriter(os.Stdout) // Initialize Windows 10 console coloring
 	/*
@@ -48,10 +50,11 @@ func main() {
 	fmt.Fprintf(w, text, "\x1b[37m", "\x1b[1m", "\x1b[21m", "\x1b[47;30m", "\x1b[0m") // White
 	*/
 	
-	
 	//logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
 	//logrus.SetOutput(colorable.NewColorableStdout())
 	
+	// ========== ========== ========== ========== ==========
+	// Splash Screen
 	// http://patorjk.com/software/taag/#p=display&v=1&f=Big&t=GoServe!
 	//fmt.Print(color.RedString("   _____       _____                     _"), "\n")
 	//logrus.Info("   _____       _____                     _")
@@ -72,9 +75,37 @@ func main() {
 	fmt.Print("\n")
 	fmt.Print("https://github.com/pathaugen/GoServe")
 	fmt.Print("\n\n")
+	// ========== ========== ========== ========== ==========
 	
 	
+	// ========== ========== ========== ========== ==========
+	// Command List
+	// TODO: Convert to JSON or another static command/description and format accoringly
+	fmt.Print("GoServe! Console Commands:")
+	fmt.Print("\n")
+	fmt.Fprintf(consoleColor, "\x1b[36m\x1b[1m") // Cyan, bold
+	fmt.Print("'monitor'")
+	fmt.Fprintf(consoleColor, "\x1b[0m") // Reset colors, bold
+	fmt.Print("	Start web monitoring of visitors.")
+	fmt.Print("\n")
+	fmt.Fprintf(consoleColor, "\x1b[36m\x1b[1m") // Cyan, bold
+	fmt.Print("'help'")
+	fmt.Fprintf(consoleColor, "\x1b[0m") // Reset colors, bold
+	fmt.Print("		Open help documentation.")
+	fmt.Print("\n")
+	fmt.Fprintf(consoleColor, "\x1b[36m\x1b[1m") // Cyan, bold
+	fmt.Print("'exit'")
+	fmt.Fprintf(consoleColor, "\x1b[0m") // Reset colors, bold
+	fmt.Print("		Exit the application.")
+	fmt.Print("\n\n")
+	// ========== ========== ========== ========== ==========
 	
+	
+	// ========== ========== ========== ========== ==========
+	// Multiple webservers can be created and started
+	// > webserver list -> List all running webservers
+	// > webserver start 8080 -> Start a webserver on the specified port
+	// webservers.cfg -> configuration file with webservers that will be run on startup
 	fmt.Print("Starting Webserver on port "+port+".. please wait..")
 	fmt.Print("\n")
 	//http.HandleFunc("/", handler)
@@ -93,37 +124,18 @@ func main() {
 	fmt.Print("\n")
 	fmt.Print("Visit: http://localhost:"+port)
 	fmt.Print("\n\n")
-	
-	
-	// Command List
-	fmt.Print("GoServe! Console Commands:")
-	fmt.Print("\n")
-	fmt.Fprintf(consoleColor, "\x1b[36m\x1b[1m") // Cyan, bold
-	fmt.Print("'monitor'")
-	fmt.Fprintf(consoleColor, "\x1b[0m") // Reset colors, bold
-	fmt.Print("	Start web monitoring of visitors.")
-	fmt.Print("\n")
-	fmt.Fprintf(consoleColor, "\x1b[36m\x1b[1m") // Cyan, bold
-	fmt.Print("'help'")
-	fmt.Fprintf(consoleColor, "\x1b[0m") // Reset colors, bold
-	fmt.Print("		Open help documentation.")
-	fmt.Print("\n")
-	fmt.Fprintf(consoleColor, "\x1b[36m\x1b[1m") // Cyan, bold
-	fmt.Print("'exit'")
-	fmt.Fprintf(consoleColor, "\x1b[0m") // Reset colors, bold
-	fmt.Print("		Exit the application.")
-	fmt.Print("\n\n")
+	// ========== ========== ========== ========== ==========
 	
 	
 	// Every 10 seconds save log files
 	// xxx
 	
-	
+	// Loop listening for input via inputHandler
 	for !exit {
 		inputHandler()
 	}
 	
-	
+	// If exit is set to true, gracefully exit via this code
 	if exit {
 		// Save command log file: goserve-commands.log
 		// xxx
@@ -133,9 +145,5 @@ func main() {
 		
 		exitHandler()
 	}
-	
-	
-    
-	
 }
 
